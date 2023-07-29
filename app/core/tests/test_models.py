8fc782e69853f5e073530f0a25028b9552a7a6e0
Lwazi <lwazi@wethinkcode.co.za>
2023-07-30 00:50:53 +0200

    implementing user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """
+    Tests core models
+    """
+
+
+    def test_create_user_with_email_successful(self):
+        """
+        Tests creating a user successfully with a new email
+        address
+        """
+
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
